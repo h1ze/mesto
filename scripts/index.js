4b23b699707f9d954c5a182e6e19a39e36bd1f34
h1ze <burgov@mail.ru>
2022-11-24 14:08:34 +0400

    fix: Исправить объявление и названия переменных

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
-let editBtn = document.querySelector(".profile__btn-edit");
-let popup = document.querySelector(".popup");
-let closeBtn = document.querySelector(".popup__close");
-let formElement = document.querySelector(".form");
-let profileName = document.querySelector(".profile__title");
-let profileInfo = document.querySelector(".profile__subtitle");
-let inputName = formElement.querySelector(".form__input_value_name");
-let inputInfo = formElement.querySelector(".form__input_value_info");
+const btnEdit = document.querySelector(".profile__btn-edit");
+const popup = document.querySelector(".popup");
+const btnClose = document.querySelector(".popup__close");
+const formElement = document.querySelector(".form");
+const profileName = document.querySelector(".profile__title");
+const profileInfo = document.querySelector(".profile__subtitle");
+const inputName = formElement.querySelector(".form__input_value_name");
+const inputInfo = formElement.querySelector(".form__input_value_info");
 
 function popupOpen() {
   popup.classList.add("popup_opened");
@@ -17,9 +17,8 @@ function popupClose() {
   popup.classList.remove("popup_opened");
 }
 
-editBtn.addEventListener("click", popupOpen);
-
-closeBtn.addEventListener("click", popupClose);
+btnEdit.addEventListener("click", popupOpen);
+ btnClose.addEventListener("click", popupClose);
 
 function formSubmitHandler(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
