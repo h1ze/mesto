8babed3df4d824138718108fe9cf0cc771ce5108
h1ze <burgov@mail.ru>
2022-12-17 03:24:49 +0400

    feat: реализовать механику активности кнопки

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,13 +1,42 @@
-const inputs = document.querySelectorAll('.form__input');
-inputs.forEach(input => {
-    input.addEventListener('input', () => {
-        const error = document.querySelector(`.${input.id}-error`);
-        if (input.validity.valid) {
-            //убрать ошибку
-            error.textContent = '';
-        } else {
-            //показать ошибку
-            error.textContent = 'Ошибка!';
-        }
-    })
-})
+const forms = [...document.querySelectorAll('.form')];
+
+
+
+forms.forEach(form => {
+    const inputs = [...document.querySelectorAll('.form__input')];
+    const button = form.querySelector('.form__button');
+    console.log(button);
+    form.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+    });
+    inputs.forEach(input => {
+        input.addEventListener('input', () => {
+            const error = document.querySelector(`.${input.id}-error`);
+            // console.log(`.${input.id}`);
+            // console.log(`.${input.id}-error`);
+            if (input.validity.valid) {
+                //убрать ошибку
+                error.textContent = '';
+            } else {
+                //показать ошибку
+                error.textContent = input.validationMessage;
+            }
+
+            const isFormValid = inputs.every(input =>  input.validity.valid);
+            if (isFormValid) {
+                console.log("Отключить");
+                button.classList.remove('form__button_status_disabled');
+            } else {
+                button.classList.add('form__button_status_disabled');
+            }
+        });
+    });
+});
+
+
+
+
+
+
+
+
