368785a17ce4b6685a6300ffc3e7d94798a58847
h1ze <burgov@mail.ru>
2022-12-17 14:21:03 +0400

    feat: переделать структуру по вебинару

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,41 +1,59 @@
-const forms = [...document.querySelectorAll('.form')];
 
 
+function checkInputValidity(input, config) {
+    const error = document.querySelector(`.${input.id}-error`);
+    if (input.validity.valid) {
+        //убрать ошибку
+        error.textContent = '';
+        error.classList.remove(ErrorClass);
+    } else {
+        //показать ошибку
+        error.textContent = input.validationMessage;
+        error.classList.add(ErrorClass);
+    }
+};
 
-forms.forEach(form => {
-    const inputs = [...document.querySelectorAll('.form__input')];
-    const button = form.querySelector('.form__button');
-    console.log(button);
-    form.addEventListener('submit', (evt) => {
-        evt.preventDefault();
-    });
-    inputs.forEach(input => {
-        input.addEventListener('input', () => {
-            const error = document.querySelector(`.${input.id}-error`);
-            // console.log(`.${input.id}`);
-            // console.log(`.${input.id}-error`);
-            if (input.validity.valid) {
-                //убрать ошибку
-                error.textContent = '';
-            } else {
-                //показать ошибку
-                error.textContent = input.validationMessage;
-            }
+function toggleButton(inputs, button, config) {
+    const isFormValid = inputs.every(input =>  input.validity.valid);
+    
+    if (isFormValid) {
+        console.log("Отключить");
+        button.classList.remove(config.inactiveButtonClass);
+    } else {
+        button.classList.add(config.inactiveButtonClass);
+    };
+};
 
-            const isFormValid = inputs.every(input =>  input.validity.valid);
-            if (isFormValid) {
-                console.log("Отключить");
-                button.classList.remove('form__button_status_disabled');
-            } else {
-                button.classList.add('form__button_status_disabled');
-            }
+            
+function enableValidation(config) {
+    const forms = [...document.querySelectorAll(config.formSelector)];
+
+    forms.forEach(form => {
+        const inputs = [...form.querySelectorAll(config.inputSelector)];
+        const button = form.querySelector(config.submitButtonSelector);
+
+        form.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+        });
+
+        inputs.forEach(input => {
+            input.addEventListener('input', () => {
+                checkInputValidity(input, config);
+                toggleButton(inputs,button,config);
+            })
         });
     });
-});
+};
 
 
-
-
+enableValidation({
+    formSelector: '.form',
+    inputSelector: '.form__input',
+    submitButtonSelector: '.form__button',
+    inactiveButtonClass: '.form__button_status_disabled',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__input-error_active'
+  });
 
 
 
