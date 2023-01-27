df07c64ae30ad14afe5ebf23ad83dd4a0591b84b
h1ze <burgov@mail.ru>
2023-01-27 12:33:45 +0400

    fix: изменить приватные методы open&close

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -1,36 +1,35 @@
 export class Popup {
     constructor(popupSelector) {
         this._popup = document.querySelector(popupSelector);
-        this._btnCLose = this._popup.querySelector('.popup__close');
+        this._buttonCloseCLose = this._popup.querySelector('.popup__close');
     }
 
-    openPopup(popup) {
-        popup.classList.add("popup_opened");
+    open() {
+        this._popup.classList.add("popup_opened");
         document.addEventListener('keyup', () => this._handleEscUp);
     }
 
-    closePopup(popup) {
-        popup.classList.remove("popup_opened");
+    close() {
+        this._popup.classList.remove("popup_opened");
         document.removeEventListener('keyup', () => this._handleEscUp);
     }
 
     _handleEscUp(evt) {
         if (evt.key === 'Escape') {
             const currentOpenPopup = document.querySelector('.popup_opened');
-            this.closePopup(currentOpenPopup);
+            t(currentOpenPopup);
         }
     }
 
     _handleClickOutside(evt) {
-        if (evt.target === evt.currentTarget) {
-          closePopup(evt.currentTarget);
+        if (evt.target === evt.currentTarget) {(evt.currentTarget);
         }
     }
 
-    setEventListeners(btn) {
-        const currentOpenPopup = btn.closest('.popup');
+    setEventListeners(buttonClose) {
+        const currentOpenPopup = buttonClose.closest('.popup');
         currentOpenPopup.addEventListener('mousedown', () => this._handleClickOutside);
-        btn.addEventListener('click', () => this.closePopup(currentOpenPopup)); 
+        buttonClose.addEventListener('click', () => t(currentOpenPopup)); 
     }
 
 
