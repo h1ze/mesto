35e7a07101bf1fe93cbf29c107382664c0968826
h1ze <burgov@mail.ru>
2023-01-29 15:41:24 +0400

    feat: Добавить наброски класса PopupWithForm

--- scripts/PopupWithForm.js
+++ scripts/PopupWithForm.js
@@ -3,6 +3,30 @@ import { Popup } from "./Popup.js";
 export class PopupWithForm extends Popup {
     constructor(popupSelector, handleFormSubmit) {
         super(popupSelector);
+        this._handleFormSubmit = handleFormSubmit;
+        this._form = this._popup.querySelector('.form');
+        this._inputs = [...this._form.querySelectorAll(".form__input")];
         
+    }
+
+    _getInputValues() {
+        this._inputValues = {};
+        this._inputList.forEach((input) => {
+            this._inputValues[input.name] = input.value;
+        });
+        return this._inputValues;
+    }
+
+    setEventListeners() {
+        super.setEventListeners();
+        this._form.addEventListener('submit', () => {
+            evt.preventDefault();
+            this._handleFormSubmit(this._getInputValues());
+        });
+    }
+
+    close() {
+        super.close();
+        this._form.reset();
     }
 }
