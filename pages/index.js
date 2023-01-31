70aeec93f217c193e757525ff85233ea74e7d5d7
h1ze <burgov@mail.ru>
2023-01-31 14:26:59 +0400

    feat: вынести константы из index.js

--- pages/index.js
+++ pages/index.js
@@ -5,52 +5,7 @@ import { Popup } from '../components/Popup.js';
 import { PopupWithImage } from '../components/PopupWithImage.js';
 import { PopupWithForm } from '../components/PopupWithForm.js';
 import { UserInfo } from '../components/UserInfo.js';
-
-const btnEditProfile = document.querySelector(".profile__btn-edit");
-const btnAddCard = document.querySelector(".profile__btn-add");
-const profileForm = document.forms["profile-form"];
-const cardForm = document.forms["card-form"];
-const inputName = profileForm.querySelector(".form__input_value_name");
-const inputInfo = profileForm.querySelector(".form__input_value_info");
-const cardsList = document.querySelector(".elements__list");
-
-
-const constConfig = {
-  formSelector: '.form',
-  inputSelector: '.form__input',
-  submitButtonSelector: '.form__button',
-  inactiveButtonClass: 'form__button_status_disabled',
-  inputErrorClass: 'form__input_state_error',
-  errorClass: 'form__error_active',
-};
-
-// Массив карточек
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-]; 
+import { btnEditProfile, btnAddCard, profileForm, cardForm, inputName, inputInfo, cardsList, constConfig, initialCards } from '../utils/constants.js';
 
 // создание обьекта с редактируемыми текстовыми полями профиля
 
@@ -68,6 +23,7 @@ const formCardAdd = new FormValidator(cardForm, constConfig);
 formCardAdd.enableValidation();
 
 // Создание экземпляра карточки
+
 const createCard = (data) => {
   const cardObj = new Card(data, '#card', () => popupWithImage.open(data.name, data.link));
   const cardElement =  cardObj.createCard();
@@ -91,8 +47,6 @@ cardsListSection.renderItems();
 
 const popupWithImage = new PopupWithImage('.popup_menu_image');
 popupWithImage.setEventListeners();
-
-
 
 // Создание попапа редактирования профиля
 
---
+++ utils/constants.js
@@ -0,0 +1,43 @@
+export const btnEditProfile = document.querySelector(".profile__btn-edit");
+export const btnAddCard = document.querySelector(".profile__btn-add");
+export const profileForm = document.forms["profile-form"];
+export const cardForm = document.forms["card-form"];
+export const inputName = profileForm.querySelector(".form__input_value_name");
+export const inputInfo = profileForm.querySelector(".form__input_value_info");
+export const cardsList = document.querySelector(".elements__list");
+
+export const constConfig = {
+    formSelector: '.form',
+    inputSelector: '.form__input',
+    submitButtonSelector: '.form__button',
+    inactiveButtonClass: 'form__button_status_disabled',
+    inputErrorClass: 'form__input_state_error',
+    errorClass: 'form__error_active',
+  };
+
+export const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
